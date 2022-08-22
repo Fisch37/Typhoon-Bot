a77b7cfedfdd852cc6a3a2d5090aeed354f30d04
Fisch37 <seracin4@gmail.com>
2022-08-22 08:43:42 +0200

    Moved extensions to be loaded after connect

--- main.py
+++ main.py
@@ -168,18 +168,18 @@ async def main():
             pass
         pass
 
-    await asyncio.gather(
-        BOT.load_extension("fun"),
-        # BOT.load_extension("utility"),
-        # BOT.load_extension("moderation"),
-        BOT.load_extension("leveling"),
-        
-        BOT.load_extension("help_ext"),
-        # BOT.load_extension("config"),
-    )
-
     try:
         async with BOT:
+            await asyncio.gather(
+                BOT.load_extension("fun"),
+                # BOT.load_extension("utility"),
+                # BOT.load_extension("moderation"),
+                BOT.load_extension("leveling"),
+                
+                BOT.load_extension("help_ext"),
+                # BOT.load_extension("config"),
+            )
+
             await BOT.start(TOKEN)
             pass
     finally: 
